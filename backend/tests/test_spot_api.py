858b227e6fd8d5625d168f881d9cb56f804e034f
Copojacaab <palermijacopo@gmail.com>
2026-08-21 15:24:43 +0200

    feat (3.4) - API Spot Obbiettivo: esporre CRUD spot tramite FastAPI

---
+++ backend/tests/test_spot_api.py
@@ -0,0 +1,9 @@
+"""
+    - creazione riuscita: 201
+    - lettura riuscita: 200
+    - aggiornamento riuscito: 200
+    - cancellazione riuscita: 204
+    - spot inesistente o appartenente a un altro utente: 404
+    - token assente, invalido o scaduto: 401
+    - dati JSON non validi: 422
+"""
